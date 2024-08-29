c11b0c517d4e4aad4934e65faa5f7d40f2ef830a
Prabha Paul <143183441+prabha1503@users.noreply.github.com>
2024-08-29 10:52:32 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,16 @@
+package com.verizon;
+
+public class Whileloop {
+	
+	public static void main(String[] args) {
+		int x=0;
+		while(x<=10) {
+			if(x==4);
+			break;
+		System.out.println("x");
+			x+=2;
+		
+		}
+	}
+
+}
